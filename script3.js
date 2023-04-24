cc35a2ab812218107688e493b9d203288eaa2678
venkat-rtr7179 <125110848+venkat-rtr7179@users.noreply.github.com>
2023-04-24 19:08:37 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,51 @@
+Task : 1 : 
+fix.html
+<!DOCTYPE html>
+<html>
+<body>
+ <script src=”script.js”></script>
+</body>
+</html>
+and inside script.js invoke alert
+alert(“I’m invoked!”);
+
+Task : 3 :
+let fname = "Guvi";
+let lname = "geek"
+let name = fname+lname;
+alert( 'hello ${name}' );
+
+Task : 4 :let a = prompt("First number?");
+let b = prompt("Second number?");
+alert(a + b);
+
+Task : 6 : The user can enter anything except number like string , characters etc and it will print success
+Task :  7 : Inside console.log in each line we can print value , and can get the correct score
+Task : 8 : let login = 'Employee';
+let message = (login == 'Employee') ? :"Welcome " : '';
+console.log(message);
+Task : 9 : 
+let a = "boss";
+let message;
+if (a == "boss")
+{
+ let message = "welcome boss";
+}
+else
+{
+ let message = "Go away";
+}
+  console.log(message);
+
+Task : 11 :  Change this console.log( --i ) to console.log( i-- )
+Task : 12 : for(let i = 1 ; i<= 10 ; i++){
+                  console.log(i);
+                 }
+Task : 13 : 
+  for (let num = 2; num <= 20 && num%2 == 0; num += 1) {
+  console.log(num)
+   }
+Task : 14: 
+for (let i = 0; i < 3; i++) {
+  console.log(  gifts[i],"and added a bow!');
+}
